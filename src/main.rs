c844e6a36d2f07d0bc5b18be3d7176d013f1d5d0
Patrick Stover <patrick.n.stover@gmail.com>
2025-05-26 15:56:57 -0400

    general error handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use mwbot::Result;
+use std::error::Error;
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let bot = mwbot::Bot::from_default_config().await.unwrap();
+async fn main() -> Result<(), Box<dyn Error>> {
+    let bot = mwbot::Bot::from_default_config().await?;
     let resp = bot
         .api()
         .get_value(&[
@@ -11,14 +12,24 @@ async fn main() -> Result<()> {
             ("prop", "links"),
             ("titles", "Rust_(programming_language)"),
         ])
-        .await
-        .unwrap();
+        .await?;
     for link in resp["query"]["pages"][0]["links"]
         .as_array()
         .unwrap()
         .into_iter()
     {
         println!("{}", link["title"]);
+        let lead = bot
+            .page(link["title"].as_str().unwrap())?
+            .html()
+            .await?
+            .into_mutable()
+            .select("section > p")
+            .unwrap()
+            .next()
+            .unwrap()
+            .text_contents();
+        println!("{lead}");
     }
     Ok(())
 }
